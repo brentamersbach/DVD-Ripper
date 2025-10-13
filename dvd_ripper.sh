a790ef33e5de538100d79ee352c4f14054392551
Brent Amersbach <triumshockwave@gmail.com>
2025-10-12 22:10:17 -0400

    Added top banner, improved error handling, text formatting improvements

--- dvd_ripper.sh
+++ dvd_ripper.sh
@@ -2,6 +2,11 @@
 
 # set -x
 
+# Dependencies:
+# macOS
+# bash 4.x
+# MakeMKV
+
 # Global config
 temp_dir="/Users/bamersbach/Desktop/MakeMKV"
 targetServer="bamersbach@server.mac-anu.com"
@@ -18,20 +23,30 @@ error=""
 termHeight=""
 termWidth=""
 scrollHeight=""
+bannerMsg="DVD Ripper"
 
 function initTerm {
 	termHeight=$(tput lines)
 	termWidth=$(tput cols)
 	scrollHeight=$(($termHeight - 3))
-	printf "\e[?1049h"				# Enable alternate screen buffer
-	printf '\e[1;'$scrollHeight'r'	# Set scroll region
+	local bannerLen=${#bannerMsg}
+	local spacing=$(bc <<< "(${termWidth}-${bannerLen})/2-1")
+	printf '\e[0J'					# Clear to end of screen to clean up any artifacts
+	printf '\e[0;0H'				# Move to top left corner
+	# Print banner
+	printf '\e[0;30m\e[107m\e[1m \e[%sb%s \e[%sb\e[0K\e[0m\n' \
+		$spacing "${bannerMsg}" $spacing
+	printf '\e[3;'$scrollHeight'r'	# Set scroll region
+	# Move to start of third line and reset colors
+	printf '\e[49m\e[39m\e[22m\e[3;0H'				
 }
 
 function deinitTerm {
 	printf "\e[?25h"				# Show cursor
 	printf '\e[49m\e[39m'			# Restore default colors
-	printf '\e[?1049l'				# Disable alternate screen buffer
 	printf '\e[1;'$termHeight'r'	# Reset scroll region
+	printf '\e[?1049l'				# Disable alternate screen buffer
+
 }
 
 function exitScript {
@@ -39,7 +54,7 @@ function exitScript {
 	[[ $(pgrep makemkvcon) ]] && pkill makemkvcon
 	deinitTerm
 	
-	[[ -z "${error}" ]] || printf '\e[31m\e[1mFatal: %s\e[0m\n\n' "${error}"
+	[[ -z "${error}" ]] || printf '\n\e[31m\e[1mFatal: %s\e[0m\n' "${error}"
 }
 
 function setup {
@@ -60,18 +75,19 @@ function ripConfig {
         | grep "Volume Name" \
         | xargs \
         | cut -d ' ' -f 3-)"
+    [[ "${PIPESTATUS[0]}" -eq 0 ]] || (echo "Disc not ready" ; return 1)
     outfile="${temp_dir}/$volume_name.iso"
     discId="$(makemkvcon -r info disc | grep ${source_device} | \
     	cut -d ',' -f 1 | tail -c 2)"
 
 	# Confirming settings
-    printf -- "\nSource device: ${source_device}\n"
+    printf -- "Source device: ${source_device}\n"
     printf -- "Output file: ${outfile}\n"
     printf -- "Disc ID: ${discId}\n"
     
     # Get output file name
     local response=""
-    read -t 15 -p "Press return to continue or n to rename file: " response
+    read -t 15 -p $'\e[34m\e[1mPress return to continue or n to rename file: \e[0m' response
     if [[ "$response" = "n" ]] ; then
     	renameOutput
     fi
@@ -121,9 +137,9 @@ function progress {
 }
 
 function rip {
-	ripConfig
+	ripConfig || (echo "Rip config failure" ; return 1)
 	
-    printf -- "\nRipping $volume_name...\n"
+    printf -- "\n\e[1mRipping $volume_name...\e[22m\n"
     local starttime="$(date "+%s")"
     diskutil unmount /dev/$source_device
 	
@@ -143,19 +159,19 @@ function rip {
     
     progress
     
-    local endtime="$(date "+%s")"
-    local difftime=$(("$endtime"-"$starttime"))
-    difftime=$(bc <<< "scale=2;$difftime/60")
-    local fileSize="$(stat -f %z "$outfile")"
-    fileSize=$(bc <<< "scale=2;$fileSize/1024/1024/1024")
-    printf "\n\e[32m\e[1mRipped %sGB in %s minutes\e[0m\n\n" $fileSize $difftime
-    
     # MakeMKV doesn't return non-zero on failure, so we need to check the messages
     grep -i "Backup done" "${temp_dir}/messages.txt"
     if [[ $? != 0 ]] ; then
     	printf "\n\e[31m\e[1mMakeMKV seems to have failed, check messages\e[0m\n\n"
     	return 1
     fi
+    
+    local endtime="$(date "+%s")"
+    local difftime=$(("$endtime"-"$starttime"))
+    difftime=$(bc <<< "scale=2;$difftime/60")
+    local fileSize="$(stat -f %z "$outfile")"
+    fileSize=$(bc <<< "scale=2;$fileSize/1024/1024/1024")
+    printf "\n\e[32m\e[1mRipped %sGB in %s minutes\e[0m\n\n" $fileSize $difftime
 }
 
 function renameOutput {
@@ -165,7 +181,7 @@ function renameOutput {
 }
 
 function copy {
-    printf -- "\nUploading $volume_name...\n"
+    printf -- "\n\e[1mUploading $volume_name...\e[22m\n"
     scp -i "${sshKey}" "${outfile}" "${targetServer}:${targetDir}/"
     if [[ $? != 0 ]] ; then
 		printf '\n\e[31m\e[1mRemote copy failed\e[0m\n\n'
@@ -177,23 +193,24 @@ function copy {
 
 trap exitScript EXIT
 
-	initTerm
-	setup
-	
-	# Main loop
-	while true ; do
-		rip
-		copy || read -p "Copy failure, continue?"
-		diskutil eject $source_device
-		read -p $'\n\e[34m\e[1mPress return to continue\e[0m' continue
-		if [[ -z $continue ]] ; then
-			printf -- "\n------------\n"
-			continue
-		else
-			break
-		fi
-	done
-	
-	deinitTerm
+printf "\e[?1049h"	# Enable alternate screen buffer
+initTerm
+setup
+
+# Main loop
+while true ; do
+	rip && (copy || read -p "Copy failure, continue?")
+	read -p $'\e[34m\e[1mPress return to eject disc\e[0m\a'
+	diskutil eject $source_device
+	read -p $'\n\e[34m\e[1mPress return to continue\e[0m' continue
+	if [[ -z $continue ]] ; then
+		printf '\n\e(0q\e[%sb\e(2\n\n' $(("$termWidth"-1))	# Horizontal divider line
+		continue
+	else
+		break
+	fi
+done
+
+deinitTerm
 
 exit 0
